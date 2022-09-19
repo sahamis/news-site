eb7457a7ed605fc4976da0163677c0d8d82fc498
Your Name <you@example.com>
2022-09-19 17:29:07 +0900

    second news site foundation

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class Article(models.Model):
+    title=models.CharField(max_length=100)
+    text=models.TextField()
+    published_date=models.CharField(max_length=100)
+    category=models.CharField(max_length=100)
--- news/views.py
+++ news/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def article_list(request):
+    return render(request,'news/article_list.html',{})  
